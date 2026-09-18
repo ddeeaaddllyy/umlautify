0b7a5eda0320e9f92df1e7c7a1c0ec2e40140203
ddeeaaddllyy <naumova070783@gmail.com>
2026-09-18 16:26:14 +0300

    0.0.2

--- main.cpp
+++ main.cpp
@@ -5,10 +5,11 @@
 static HHOOK g_hook = nullptr;
 static FILE* g_log = nullptr;
 
-static bool g_altPhysicallyDown = false;
+static bool g_ctrlPhysicallyDown = false;
+static bool g_winPhysicallyDown = false;
 static bool g_composeActive = false;
 static bool g_swallowNextAltUp = false;
-static bool g_swallowNextEUp = false;
+static bool g_swallowNextWinUp = false;
 
 static void
 SendUnicodeChar(const wchar_t ch)
@@ -34,11 +35,11 @@ EmitChar(const wchar_t ch)
 {
     SendUnicodeChar(ch);
     g_composeActive = false;
-    g_swallowNextEUp = false;
+    g_swallowNextWinUp = false;
 }
 
 LRESULT CALLBACK
-LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
+LowLevelKeyboardProc(const int nCode, const WPARAM wParam, const LPARAM lParam)
 {
     if (nCode != HC_ACTION) return CallNextHookEx(g_hook, nCode, wParam, lParam);
 
@@ -47,21 +48,26 @@ LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
     const bool isUp   = (wParam == WM_KEYUP   || wParam == WM_SYSKEYUP);
     const bool injected = (kb->flags & LLKHF_INJECTED) != 0;
 
-    if (g_log) {
-        fprintf(g_log, "vk=%02X down=%d up=%d inj=%d\n", kb -> vkCode, isDown, isUp, injected);
+    if (g_log)
+    {
+        fprintf(g_log, "vk=%lu down=%d up=%d inj=%d\n", kb->vkCode, isDown, isUp, injected);
         fflush(g_log);
     }
 
     if (injected) return CallNextHookEx(g_hook, nCode, wParam, lParam);
 
-    if (kb->vkCode == VK_LMENU || kb->vkCode == VK_RMENU || kb->vkCode == VK_MENU) {
-        if (isDown) {
-            g_altPhysicallyDown = true;
+    if (kb->vkCode == VK_LCONTROL || kb->vkCode == VK_RCONTROL || kb->vkCode == VK_CONTROL)
+    {
+        if (isDown)
+        {
+            g_ctrlPhysicallyDown = true;
             return CallNextHookEx(g_hook, nCode, wParam, lParam);
         }
-        if (isUp) {
-            g_altPhysicallyDown = false;
-            if (g_swallowNextAltUp) {
+        if (isUp)
+        {
+            g_ctrlPhysicallyDown = false;
+            if (g_swallowNextAltUp)
+            {
                 g_swallowNextAltUp = false;
                 if (g_log) fprintf(g_log, "Swallowed Alt up\n");
                 return 1;
@@ -71,36 +77,50 @@ LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
         return CallNextHookEx(g_hook, nCode, wParam, lParam);
     }
 
-    if (isDown) {
-        if (!g_composeActive && kb->vkCode == 'E' && g_altPhysicallyDown) {
-            g_composeActive = true;
-            g_swallowNextAltUp = true;
-            g_swallowNextEUp = true;
+    if (kb->vkCode == VK_LWIN || kb->vkCode == VK_RWIN) {
+        if (isDown)
+        {
+            g_winPhysicallyDown = true;
 
-            INPUT altUp = {};
-            altUp.type = INPUT_KEYBOARD;
-            altUp.ki.wVk = VK_MENU;
-            altUp.ki.dwFlags = KEYEVENTF_KEYUP;
-            SendInput(1, &altUp, sizeof(INPUT));
+            if (!g_composeActive && g_ctrlPhysicallyDown)
+            {
+                g_composeActive = true;
+                g_swallowNextAltUp = true;
+                g_swallowNextWinUp = true;
 
-            if (g_log) fprintf(g_log, "Compose started, injected Alt up\n");
-            return 1;
+                INPUT ctrlUp = {};
+                ctrlUp.type = INPUT_KEYBOARD;
+                ctrlUp.ki.wVk = VK_CONTROL;
+                ctrlUp.ki.dwFlags = KEYEVENTF_KEYUP;
+                SendInput(1, &ctrlUp, sizeof(INPUT));
+
+                if (g_log) fprintf(g_log, "Compose started (Alt+Win)\n");
+                return 1;
+            }
+
+            return CallNextHookEx(g_hook, nCode, wParam, lParam);
         }
-
-        if (g_composeActive) {
-            if (kb->vkCode == 'O') { EmitChar(L'\u00F6'); return 1; }
-            if (kb->vkCode == 'S') { EmitChar(L'\u00DF'); return 1; }
-            if (kb->vkCode == 'U') { EmitChar(L'\u00FC'); return 1; }
-            if (kb->vkCode == 'A') { EmitChar(L'\u00E4'); return 1; }
-            g_composeActive = false;
+        if (isUp)
+        {
+            g_winPhysicallyDown = false;
+            if (g_swallowNextWinUp)
+            {
+                g_swallowNextWinUp = false;
+                if (g_log) fprintf(g_log, "Swallowed Win up\n");
+                return 1;
+            }
+            return CallNextHookEx(g_hook, nCode, wParam, lParam);
         }
+        return CallNextHookEx(g_hook, nCode, wParam, lParam);
     }
 
-    if (isUp) {
-        if (g_swallowNextEUp && kb->vkCode == 'E') {
-            g_swallowNextEUp = false;
-            return 1;
-        }
+    if (isDown && g_composeActive)
+    {
+        if (kb->vkCode == 'O') { EmitChar(L'\u00F6'); return 1; }
+        if (kb->vkCode == 'S') { EmitChar(L'\u00DF'); return 1; }
+        if (kb->vkCode == 'U') { EmitChar(L'\u00FC'); return 1; }
+        if (kb->vkCode == 'A') { EmitChar(L'\u00E4'); return 1; }
+        g_composeActive = false;
     }
 
     return CallNextHookEx(g_hook, nCode, wParam, lParam);
@@ -111,7 +131,8 @@ static DWORD g_mainThreadId = 0;
 static BOOL WINAPI
 ConsoleCtrlHandler(const DWORD type)
 {
-    if (type == CTRL_C_EVENT || type == CTRL_CLOSE_EVENT) {
+    if (type == CTRL_C_EVENT || type == CTRL_CLOSE_EVENT)
+    {
         PostThreadMessage(g_mainThreadId, WM_QUIT, 0, 0);
         return TRUE;
     }
@@ -119,40 +140,40 @@ ConsoleCtrlHandler(const DWORD type)
 }
 
 static bool DEBUG = false;
+static FILE* file = nullptr;
 
 int
 main(const int argc, char* argv[])
 {
     // in future
-    for (int i = 1; i < argc; i++) {
+    for (int i = 1; i < argc; i++)
+    {
         auto sargv = std::string(argv[i]);
-        if (sargv == "--help" or sargv == "-h") {
-            break;
-        }
+        if (sargv == "--help" or sargv == "-h") return 0;
         if (sargv == "--version" or sargv == "-v") {}
-        if (sargv == "--debug" or sargv == "-d") {
-            DEBUG = true;
-        }
+        if (sargv == "--debug" or sargv == "-d") DEBUG = true;
         if (sargv == "--autoload") {}
         if (sargv == "--disable-autoload" or sargv == "-dal") {}
     }
 
 
     g_mainThreadId = GetCurrentThreadId();
-    g_log = fopen("hook_log.txt", "w");
-    if (!g_log) return 1;
+    if (fopen_s(&file, "hook_log.txt", "w") != 0) return 1;
+    g_log = file;
 
     SetConsoleCtrlHandler(ConsoleCtrlHandler, TRUE);
 
-    g_hook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, NULL, 0);
-    if (!g_hook) {
+    g_hook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, nullptr, 0);
+    if (!g_hook)
+    {
         fprintf(g_log, "SetWindowsHookEx failed, err=%lu\n", GetLastError());
         fclose(g_log);
         return 1;
     }
 
     MSG msg;
-    while (GetMessage(&msg, nullptr, 0, 0)) {
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
